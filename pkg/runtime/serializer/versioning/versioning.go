34c28d28abd177d8ba77031cec20d499b1b78bcd
Clayton Coleman <ccoleman@redhat.com>
2016-01-30 17:55:08 -0500

    Add an experimental protobuf serializer
    Provide a core protobuf serializer that can either write objects with
    an envelope (a 4 byte prefix and a runtime.Object) or raw to a byte
    array.
    
    Makes a few small refactors to prepare for streaming codecs (watch)
    including a new constructor for the codec factory.

--- pkg/runtime/serializer/versioning/versioning.go
+++ pkg/runtime/serializer/versioning/versioning.go
@@ -28,18 +28,20 @@ import (
 func NewCodecForScheme(
 	// TODO: I should be a scheme interface?
 	scheme *runtime.Scheme,
-	serializer runtime.Serializer,
+	encoder runtime.Encoder,
+	decoder runtime.Decoder,
 	encodeVersion []unversioned.GroupVersion,
 	decodeVersion []unversioned.GroupVersion,
 ) runtime.Codec {
-	return NewCodec(serializer, scheme, scheme, scheme, runtime.ObjectTyperToTyper(scheme), encodeVersion, decodeVersion)
+	return NewCodec(encoder, decoder, scheme, scheme, scheme, runtime.ObjectTyperToTyper(scheme), encodeVersion, decodeVersion)
 }
 
 // NewCodec takes objects in their internal versions and converts them to external versions before
 // serializing them. It assumes the serializer provided to it only deals with external versions.
 // This class is also a serializer, but is generally used with a specific version.
 func NewCodec(
-	serializer runtime.Serializer,
+	encoder runtime.Encoder,
+	decoder runtime.Decoder,
 	convertor runtime.ObjectConvertor,
 	creater runtime.ObjectCreater,
 	copier runtime.ObjectCopier,
@@ -48,11 +50,12 @@ func NewCodec(
 	decodeVersion []unversioned.GroupVersion,
 ) runtime.Codec {
 	internal := &codec{
-		serializer: serializer,
-		convertor:  convertor,
-		creater:    creater,
-		copier:     copier,
-		typer:      typer,
+		encoder:   encoder,
+		decoder:   decoder,
+		convertor: convertor,
+		creater:   creater,
+		copier:    copier,
+		typer:     typer,
 	}
 	if encodeVersion != nil {
 		internal.encodeVersion = make(map[string]unversioned.GroupVersion)
@@ -71,11 +74,12 @@ func NewCodec(
 }
 
 type codec struct {
-	serializer runtime.Serializer
-	convertor  runtime.ObjectConvertor
-	creater    runtime.ObjectCreater
-	copier     runtime.ObjectCopier
-	typer      runtime.Typer
+	encoder   runtime.Encoder
+	decoder   runtime.Decoder
+	convertor runtime.ObjectConvertor
+	creater   runtime.ObjectCreater
+	copier    runtime.ObjectCopier
+	typer     runtime.Typer
 
 	encodeVersion map[string]unversioned.GroupVersion
 	decodeVersion map[string]unversioned.GroupVersion
@@ -90,7 +94,7 @@ func (c *codec) Decode(data []byte, defaultGVK *unversioned.GroupVersionKind, in
 		into = versioned.Last()
 	}
 
-	obj, gvk, err := c.serializer.Decode(data, defaultGVK, into)
+	obj, gvk, err := c.decoder.Decode(data, defaultGVK, into)
 	if err != nil {
 		return nil, gvk, err
 	}
@@ -169,7 +173,7 @@ func (c *codec) Decode(data []byte, defaultGVK *unversioned.GroupVersionKind, in
 // encoding the object the first override that matches the object's group is used. Other overrides are ignored.
 func (c *codec) EncodeToStream(obj runtime.Object, w io.Writer, overrides ...unversioned.GroupVersion) error {
 	if _, ok := obj.(*runtime.Unknown); ok {
-		return c.serializer.EncodeToStream(obj, w, overrides...)
+		return c.encoder.EncodeToStream(obj, w, overrides...)
 	}
 	gvk, isUnversioned, err := c.typer.ObjectKind(obj)
 	if err != nil {
@@ -180,7 +184,7 @@ func (c *codec) EncodeToStream(obj runtime.Object, w io.Writer, overrides ...unv
 		old := obj.GetObjectKind().GroupVersionKind()
 		obj.GetObjectKind().SetGroupVersionKind(gvk)
 		defer obj.GetObjectKind().SetGroupVersionKind(old)
-		return c.serializer.EncodeToStream(obj, w, overrides...)
+		return c.encoder.EncodeToStream(obj, w, overrides...)
 	}
 
 	targetGV, ok := c.encodeVersion[gvk.Group]
@@ -226,7 +230,7 @@ func (c *codec) EncodeToStream(obj runtime.Object, w io.Writer, overrides ...unv
 		obj.GetObjectKind().SetGroupVersionKind(&unversioned.GroupVersionKind{Group: targetGV.Group, Version: targetGV.Version, Kind: gvk.Kind})
 	}
 
-	return c.serializer.EncodeToStream(obj, w, overrides...)
+	return c.encoder.EncodeToStream(obj, w, overrides...)
 }
 
 // promoteOrPrependGroupVersion finds the group version in the provided group versions that has the same group as target.
